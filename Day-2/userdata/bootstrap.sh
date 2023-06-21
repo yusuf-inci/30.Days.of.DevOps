3a88bf6edb923f05f19e4c4333ceebf9f7628caa
yusuf-inci <yusufinci@protonmail.com>
2023-06-21 14:06:25 +0300

    added tomcat and jemkins installation code

--- Day-2/userdata/bootstrap.sh
+++ Day-2/userdata/bootstrap.sh
@@ -48,14 +48,33 @@ EOF
   sudo apt install openjdk-11-jre -y
   sudo apt install maven -y
 
-  # install tomcat
-  # chmod +x tomcat_setup.sh
-  # source tomcat_setup.sh
+  ### INSTALL TOMCAT ###
+  
+  # Change directory to /opt
+  cd /opt
+  # Download Apache Tomcat
+  sudo wget https://archive.apache.org/dist/tomcat/tomcat-9/v9.0.65/bin/apache-tomcat-9.0.65.tar.gz
+  # Extract the downloaded tar file
+  sudo tar -xvf apache-tomcat-9.0.65.tar.gz
+  
+  
+  # INSTALL JENKINS 
+  sudo apt update -y
+  # sudo apt install openjdk-11-jre -y
 
-  # the tomcat_setup.sh script has executed
-  # echo "Tomcat setup completed. Continuing with other tasks."
-  # sleep 10
+  curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+    /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+  echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+    https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+    /etc/apt/sources.list.d/jenkins.list > /dev/null
+  sudo apt-get update -y 
+  sudo apt-get install jenkins -y
 
+  sudo systemctl enable jenkins
+  sudo systemctl start jenkins
+  sudo systemctl status jenkins
+    
+      
   # install jenkins & run on port 8082
   # sudo apt update -y
   # sudo apt install openjdk-11-jre -y
