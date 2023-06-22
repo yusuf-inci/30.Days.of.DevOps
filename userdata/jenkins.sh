67bd3e72889789f8f4ce6db5bd2f91cc2c9b73e1
yusuf-inci <yusufinci@protonmail.com>
2023-06-22 12:39:41 +0300

    added apache tomcat install

--- userdata/jenkins.sh
+++ userdata/jenkins.sh
@@ -9,3 +9,10 @@ sudo apt-get update
 sudo apt-get install fontconfig openjdk-11-jre -y
 sudo apt-get install jenkins -y
 sudo apt install maven -y
+
+# install apache tomcat
+sudo apt-get update
+cd /opt
+sudo wget https://archive.apache.org/dist/tomcat/tomcat-9/v9.0.65/bin/apache-tomcat-9.0.65.tar.gz
+sudo tar -xvf apache-tomcat-9.0.65.tar.gz
+sudo chmod -R 757 apache-tomcat-9.0.65
